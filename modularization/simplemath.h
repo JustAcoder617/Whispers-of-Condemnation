a55e3fc1f951532beb6c9452868d56c7b7d46965
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-03-08 20:11:05 -0300

    check_weight add

--- modularization/simplemath.h
+++ modularization/simplemath.h
@@ -1,46 +1,45 @@
 #ifndef SIMPLE_MATH_H
 #define SIMPLE_MATH_H
+
 #include <string.h>
-#include <errno.h>
 #include <stdio.h>
-static inline int Expressao(int n1, int n2, char operaçao[]){
+
+static inline int Expressao(int n1, int n2, char operacao[]) {
     
-    if (strcmp(operaçao, "su")==0){
-        int resposta=n1-n2;
-        return resposta;
-    }
-    if (strcmp(operaçao, "ad")==0)
-    {
-        int resposta=n1+n2;
-        return resposta;
-    }
-    if (strcmp(operaçao, "div")==0)
-    {
-        if (n1==0 || n2==0){
-            perror("Não é possivel dividir um número por 0. origem do erro: simplemath.h");
-            return 1;
+    if (strcmp(operacao, "su") == 0) return n1 - n2;
+    if (strcmp(operacao, "ad") == 0) return n1 + n2;
+    if (strcmp(operacao, "mult") == 0) return n1 * n2;
+
+    if (strcmp(operacao, "div") == 0) {
+        if (n2 == 0) {
+            fprintf(stderr, "Erro: Divisao por zero em simple_math.h\n");
+            return 0;
         }
-        int resposta=n1/n2;
-        return resposta;
+        return n1 / n2;
     }
-    
-    if (strcmp(operaçao, "mult")==0){
-        int resposta=n1*n2;
-        return resposta;
-    }
-    if (strcmp(operaçao, "pot")==0){
-        for (int i; i<n2; i++){
-            n1=n1*n1;
+
+    if (strcmp(operacao, "pot") == 0) {
+        int resultado = 1;
+        for (int i = 0; i < n2; i++) {
+            resultado *= n1;
         }
-        int resultado=n1;
         return resultado;
     }
-    if(strcmp(operaçao, "frdm")){//fração para decimal
-        float resultado=n1/n2;
-        return resultado;
+
+    if (strcmp(operacao, "frdm") == 0) {
+        return (n2 != 0) ? (n1 / n2) : 0;
     }
-    else{
-        perror("Operação inválida.");
-    }
+
+    fprintf(stderr, "Erro: Operacao '%s' invalida.\n", operacao);
+    return -1;
 }
+
+
+
+static inline int tamanho_check(int var_check, int tamanho_max) {
+    if (var_check > tamanho_max) return 2;
+    if (var_check < tamanho_max) return 0;
+    return 1;
+}
+
 #endif

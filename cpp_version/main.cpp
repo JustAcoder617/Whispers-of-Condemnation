8fd8f28942aad29c8a49be594baffa9dd917f6a2
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-03-22 16:39:46 -0300

    finish cpp version

--- cpp_version/main.cpp
+++ cpp_version/main.cpp
@@ -2,15 +2,36 @@
 #include <random>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <ctime>
+#include <curl/curl.h>
 
 using namespace std;
 
 int sorteio(int jg);
 void game(string player1);
+void avaliacao(string nome_jogador);
+string get_url_from_assets();
 
 int jg1vivo = 1;
 int jg2vivo = 1;
 int vez = 1;
+
+string get_url_from_assets() {
+    ifstream file("asset.txt");
+    string encrypted;
+    string decrypted = "";
+    if (file.is_open()) {
+        getline(file, encrypted);
+        file.close();
+        for (char c : encrypted) {
+            decrypted += c ^ 42;
+        }
+    } else {
+        cerr << "Erro: Arquivo asset.txt não encontrado!" << endl;
+    }
+    return decrypted;
+}
 
 int main(void) {
     cout << "Olá, Bem-vindo ao Whispers of condemnation (Versão c++)!\n";
@@ -27,7 +48,6 @@ void game(string player1) {
     string player2;
     cin >> player2;
     cout << "\nComeçando os jogos...\n";
-
     while (jg1vivo == 1 && jg2vivo == 1) {
         if (vez == 1) {
             cout << "\n" << player1 << ", escolha: 1.condenar 2.passar: ";
@@ -48,9 +68,9 @@ void game(string player1) {
             vez = 1;
         }
     }
-
     if (jg1vivo == 0) cout << "\n" << player2 << " GANHOU O JOGO!\n";
     else cout << "\n" << player1 << " GANHOU O JOGO!\n";
+    avaliacao(player1);
 }
 
 int sorteio(int jg) {
@@ -58,18 +78,7 @@ int sorteio(int jg) {
     mt19937 gen(rd());
     uniform_int_distribution<> distrib(1, 4);
     int s = distrib(gen);
-    
     int chance;
     if (s == 1) chance = 25;
     else if (s == 2) chance = 50;
     else if (s == 3) chance = 75;
-    else chance = 100;
-
-    uniform_int_distribution<> d100(1, 100);
-    if (d100(gen) <= chance) {
-        if (jg == 1) jg2vivo = 0;
-        else jg1vivo = 0;
-        return 1;
-    }
-    return 0;
-}
